f5006f9d4ae404e102d0630b9efbe1221388411c
riddler_rid <unkownrid@gmail.com>
2025-02-08 13:25:23 +0300

    add hotel migration

--- alembic/env.py
+++ alembic/env.py
@@ -10,6 +10,8 @@ from alembic import context
 from src.config import settings
 from src.models import Base
 
+from src.hotels.models import Hotel # noqa
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
