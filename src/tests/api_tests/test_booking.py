209a3ce3f898cda3373afe0ca7907b0b9610ece2
riddler_rid <unkownrid@gmail.com>
2025-02-14 12:49:34 +0300

    add some tests

--- src/tests/api_tests/test_booking.py
+++ src/tests/api_tests/test_booking.py
@@ -8,6 +8,18 @@ async def test_get_booking(authenticated_client: AsyncClient):
     assert response.status_code == 200
     assert len(response.json()) == 3
 
+@pytest.mark.asyncio
+async def test_get_my_booking(authenticated_client: AsyncClient):
+    response = await authenticated_client.get("/booking/my")
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+
+
+@pytest.mark.asyncio
+async def test_post_booking_unauthorized(client: AsyncClient):
+    response = await client.get("/booking/my")
+    assert response.status_code == 401
+
 
 @pytest.mark.asyncio
 async def test_post_booking(authenticated_client: AsyncClient):
@@ -15,10 +27,32 @@ async def test_post_booking(authenticated_client: AsyncClient):
         "/booking",
         params={
             "room_id": 1,
-            "date_from": "2021-01-01",
-            "date_to": "2021-01-02",
+            "date_from": "2025-02-14",
+            "date_to": "2025-02-21",
         },
     )
     assert response.status_code == 200
     assert response.json()["room_id"] == 1
     assert response.json()["id"] == 4
+
+    response = await authenticated_client.get("/booking/my")
+    assert response.status_code == 200
+    assert len(response.json()) == 3
+
+
+@pytest.mark.asyncio
+async def test_post_booking_not_available(authenticated_client: AsyncClient):
+    params = {
+        "room_id": 1,
+        "date_from": "2025-02-14",
+        "date_to": "2025-02-21",
+    }
+    response = await authenticated_client.post("/booking", params=params)
+    assert response.status_code == 200
+    response = await authenticated_client.post("/booking", params=params)
+    assert response.status_code == 409
+    assert response.json() == {"detail": "There are no rooms of this type left available."}
+
+
+
+
--- src/tests/api_tests/test_hotels.py
+++ src/tests/api_tests/test_hotels.py
@@ -7,3 +7,14 @@ async def test_get_hotels(client: AsyncClient):
     response = await client.get("/hotel")
     assert response.status_code == 200
     assert len(response.json()) == 6
+
+@pytest.mark.asyncio
+async def test_get_hotel_by_location(client: AsyncClient):
+    location = "Алтай"
+    params = {
+        "date_from": "2025-02-14",
+        "date_to": "2025-02-21",
+    }
+    response = await client.get(f"/hotel/{location}", params=params)
+    assert response.status_code == 200
+    assert len(response.json()) == 3
